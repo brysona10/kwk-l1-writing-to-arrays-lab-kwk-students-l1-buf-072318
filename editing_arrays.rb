14dfb82f005ab703dc7ddaa88354bb21727f5581
brysona10 <addisonbryson10@gmail.com>
2018-07-25 15:39:55 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -19,6 +19,6 @@ end
 # puts"Array after calling change_rainbow_colors"
 # puts RAINBOW_COLORS
 
-# def add_colors
-#   # Write a solution that adds "green", then "blue" to the rainbow_colors array, then returns the array
-# end
+def add_colors
+  # Write a solution that adds "green", then "blue" to the rainbow_colors array, then returns the array
+end
