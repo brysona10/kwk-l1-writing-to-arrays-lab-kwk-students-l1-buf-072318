b7e2adc833d69a7f6356d03dad42b8c7f61367f7
brysona10 <addisonbryson10@gmail.com>
2018-07-25 15:32:30 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -12,6 +12,8 @@ def change_rainbow_colors
 
 end
 
-def add_colors
-  # Write a solution that adds "green", then "blue" to the rainbow_colors array, then returns the array
-end
+change_rainbow_colors
+
+# def add_colors
+#   # Write a solution that adds "green", then "blue" to the rainbow_colors array, then returns the array
+# end
